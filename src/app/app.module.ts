4447ab0e852284af34ff068c51df060387c08974
Diego Alberto Z. L <diego.alberto.zaratelara@gmail.com>
2018-01-17 16:10:38 -0600

    Modificación de app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { RecetasComponent } from './recetas/recetas.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { RecetasDetalleComponent } from './recetas-detalle/recetas-detalle.component';
 import { RecetasService } from './recetas.service';
-
 
 @NgModule({
   declarations: [
