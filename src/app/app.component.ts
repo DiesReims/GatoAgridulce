029bf7777fa5865d486b036163a13774be73a523
Diego Alberto Z. L <diego.alberto.zaratelara@gmail.com>
2018-03-09 16:53:00 -0600

    Corrigiendo detalles de formularios.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gato Agridulce';
+  tokenSesionUser: string = "";
+  usernameLoged: string = "";
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 import {Login} from '../Modelos/Login';
 
@@ -9,16 +12,34 @@ import {Login} from '../Modelos/Login';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  private loginForm:any;
+  private loginForm:FormGroup;
+  objPost:any;
   private objLogin: Login;
-  constructor(fb: FormBuilder) {
+
+  constructor(fb: FormBuilder, 
+    private route: ActivatedRoute,
+    private location: Location,
+    private router: Router) {
+    this.loginForm = fb.group({
+      'strUsuario':[null, Validators.compose([Validators.required, Validators.maxLength(25)])],
+      'strPassword':[null, Validators.compose([Validators.required, Validators.maxLength(25)])]
+    })
    }
 
   ngOnInit() {
   }
 
-  logear():boolean {
-    return true;
+  logear(_post:any):void {
+    this.objPost = _post;
+    //TODO:Logica de servicio y validación...
+    //TODO:Servicio para manejar inicios de sesión.
+    if (_post.strUsuario === "diego" && _post.strPassword==="aide")
+    {
+      this.router.navigateByUrl('/main');
+    }
+    else{
+      alert("Inicio de sesión no válido.");
+    }
   }
 
 }
